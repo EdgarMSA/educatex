3736b356ba23aa1a85da6a9e87aec2afee511a9e
EdgarMSA <edgarmsotoa@gmail.com>
2025-12-09 22:45:04 -0500

    Fix axios baseURL using window.location.origin and clean API calls

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,10 @@
 // frontend/src/api/client.js
 import axios from 'axios';
 
+// Siempre usa el mismo origen donde se sirve la app (http://34.229.119.99)
 const api = axios.create({
-  baseURL: '/api', // esto usa el MISMO dominio/IP donde se sirve el frontend
+  baseURL: window.location.origin + '/api',
+  // baseURL quedará como: "http://34.229.119.99/api"
 });
 
 export default api;
